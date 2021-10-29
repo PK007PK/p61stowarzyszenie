8887634f66f1662c973142bffcc718cb7084ee9b
Piotr Krasny <krasny.piotr@gmail.com>
2021-10-29 08:20:09 +0200

    Improve pagination

--- gatsby/src/components/Pagination/Pagination.js
+++ gatsby/src/components/Pagination/Pagination.js
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import { PaginationStyle } from './Pagination.style';
 
-export default function Pagination({ pageSize, totalCount, currentPage, skip, base, className, style, location }) {
-    // make some variables
+export default function Pagination({ pageSize, totalCount, currentPage, base, className, style }) {
     const totalPages = Math.ceil(totalCount / pageSize);
-    const prevPage = currentPage - 1;
-    const nextPage = currentPage + 1;
-    const hasNextPage = nextPage <= totalPages;
-    const hasPrevPage = prevPage >= 1;
-    const checkLocation = location?.pathname.split('/');
-    const [pageNo, setPageNo] = useState();
-
-    // useEffect(() => {
-    //     location && setPageNo(location.pathname.split('/')[1]);
-    // }, []);
-
-    // const checkLocation = location.pathname.split('/')[1];
-    // console.log(pageNo?.length);
 
     return (
         <PaginationStyle style={style} className={className}>
             {Array.from({ length: totalPages }).map((_, i) => (
                 <Link
-                    className={currentPage === 1 && i === 0 ? 'current' : ''}
+                    className={currentPage === i + 1 ? 'current' : ''}
                     to={`${base}${i >= 0 ? i + 1 : ''}#blog`}
                     key={`page${i}`}
                 >
